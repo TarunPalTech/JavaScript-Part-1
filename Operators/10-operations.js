6852c65c896043902a05f60ce2c6bd712a100ac0
Tarun Pal <139231055+TarunPalTech@users.noreply.github.com>
2025-06-26 04:06:02 +0000

    First Commit

---
+++ Operators/10-operations.js
@@ -0,0 +1,86 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue); // -3
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello";
+let str2 = " tarun";
+let str3 = str1 + str2;  // concatenation
+// console.log(str3);
+// hello tarun
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// 12
+// 12
+// 122
+// 32
+
+// Best Method
+
+// console.log((3 + 4) * (5 % 3));
+// 14
+console.log(+true);
+// 1
+
+// console.log(true+);
+// Error
+
+// console.log(+"");
+// 0
+
+// let num1, num2, num3 = 2 + 2; 
+// num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;  // increment
+--gameCounter;  // decrement
+console.log(gameCounter);
+// 100
+
+
+
+/* Comparision */
+
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+// true
+// true 
+// data type at the time of comparision must be same...
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// false
+// false
+// true
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// false
+// false
+// false
+
+// strict check
+
+// console.log("2" === 2);
+// false
